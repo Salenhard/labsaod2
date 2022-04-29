0d43b8c308c1ec4b805b715c9764f595bb668d7b
VladislavGurbatov <vlad.gurbatov@gmail.com>
2022-04-29 03:35:01 +0300

    added lab 5

---
+++ laba_5_saod/laba_5_saod/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <String>
+#include <iostream>
+
+namespace dynamic {
+	void algoritm(int* x, int n);				// алгоритм динамич. программирования
+	void show(int* x, int n);					// вывод массиива в консоль
+}
---
+++ laba_5_saod/laba_5_saod/laba_5_saod.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Header.h"
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    n++;
+    int* x = new int[n];
+    dynamic::algoritm(x,n);
+    delete[]x;
+}
+
